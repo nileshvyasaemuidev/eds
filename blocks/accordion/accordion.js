bee13e31298fd200ac309e68528060998ee2c6e9
Nilesh Vyas <nileshhvyas@gmail.com>
2026-06-04 18:33:20 +0530

    Add .gitattributes and normalize line endings

---
+++ blocks/accordion/accordion.js
@@ -0,0 +1,21 @@
+export default function decorate(block) {
+     [...block.children].forEach((row) => {
+          const [question, answer] = row.children;
+          const item = document.createElement('div');
+          item.className = 'accordion-item';
+          const button = document.createElement('button');
+          button.className = 'accordion-title';
+          button.textContent = question.textContent;
+
+          const content = document.createElement('div');
+          content.className = 'accordion-content';
+          content.innerHTML = answer.innerHTML;
+
+          button.addEventListener('click', () => {
+               item.classList.toggle('open');
+          });
+
+          item.append(button, content);
+          row.replaceWith(item);
+     });
+}
